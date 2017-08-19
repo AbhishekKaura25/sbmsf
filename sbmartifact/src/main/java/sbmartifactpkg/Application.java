cc85e0d005d67820f079e21b5449f45ac848cca6
Abhishek Kaura <ctsuser271718@gmail.com>
2017-08-18 22:12:21 -0700

    Update Application.java

--- sbmartifact/src/main/java/sbmartifactpkg/Application.java
+++ sbmartifact/src/main/java/sbmartifactpkg/Application.java
@@ -1,19 +1,9 @@
 package sbmartifactpkg;
 
-/*
-import java.security.SecureRandom;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-*/
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,23 +15,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping(value="/")
-	public Map<String, String> demo() throws UnknownHostException{
-		/*List<Character> temp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars()
-	            .mapToObj(i -> (char)i)
-	            .collect(Collectors.toList());
-	    Collections.shuffle(temp, new SecureRandom());
-	    return temp.stream()
-	            .map(Object::toString)
-	            .limit(5)
-	            .collect(Collectors.joining());*/
-		
-		Map<String, String> map = new HashMap<String, String>();
-		
-		String host = InetAddress.getLocalHost().getHostAddress().toString();
-		map.put("Server", host );
-		
-		return map;
-		
+	@GetMapping("/sayhello")
+	@CrossOrigin
+	public String sayHello(){
+		return "Welcome to Abhishek world";
 	}
 }
